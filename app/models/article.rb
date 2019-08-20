efa886f11a9e89f8069d9a34b1d903531ceed7c2
girOly <girardob@gmail.com>
2019-08-20 16:51:01 -0400

    further model - route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
+  has_many :comments
   validates :title, presence: true,
                     length: { minimum: 5 }
+  resources :articles do
+    resources :comments
+  end
 end
