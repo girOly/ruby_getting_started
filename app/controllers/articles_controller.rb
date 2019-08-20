411197a722381ec79187a5639cd47c2474692650
girOly <girardob@gmail.com>
2019-08-20 16:12:47 -0400

    added input form for content

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,20 @@
 class ArticlesController < ApplicationController
-  class ArticlesController < ApplicationController
-    def new
-    end
-
-    def create
-  @article = Article.new(params.require(:article).permit(:title, :text))
-  @article.save
-  redirect_to @article
-end
+  def show
+    @article = Article.find(params[:id])
   end
-end
+ 
+  def new; end
+
+  def create
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+  end
